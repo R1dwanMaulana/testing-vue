29fc776dad5d6c0c646690c6b77cce886f5ed4eb
Ridwan Maulana <rdwanf49@gmail.com>
2020-10-13 19:11:53 +0700

    upload project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{ firstName }}</h1>
+    <h1>{{ lastName }}</h1>
+    <h1>{{ fullName }}</h1>
+    <h3>{{ counter }}</h3>
+    <button @click="incrementCounter">Press Me</button>
+  </div>
+</template>  
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+  firstName="Ridwan"
+  lastName="Maulana"
+  counter = 0;
+  zip: string;
+
+  mounted() {
+    console.log("mounted");
+  }
+
+  get fullName(): string {
+    return this.firstName + " " + this.lastName
+  }
+
+  incrementCounter(){
+    this.counter++;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
